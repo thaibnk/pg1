12efe40b60a5fc00298d3b5b09f3ff58a8f5f295
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-07-28 17:49:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
 //$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-$result = pg_query($pg_conn, "SELECT * FROM tbl");
+$result = pg_query($pg_conn, "SELECT * FROM tbl;");
 
 print "<pre>\n";
 if (!pg_num_rows($result)) {
