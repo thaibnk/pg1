e9f725a15b4d253a3d17fc5e75690532d6f8a28e
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-07-28 17:37:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
 //$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-$result = pg_query($pg_conn, "SELECT  * from tbl");
+$result = pg_query($pg_conn, "SELECT * FROM tbl");
 
 print "<pre>\n";
 if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+  print("xYour connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
   print "Tables in your database:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
